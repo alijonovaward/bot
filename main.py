281c554ffc710d60a2c6ae9fb7ee0fc679e239f5
Alijonovaward <Alijonovaward@gmail.com>
2026-01-01 18:00:10 +0500

    a

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from aiogram.types import Message
 
 # Bot token can be obtained via https://t.me/BotFather
 TOKEN = "8005783423:AAGfXjIXqA3uVO4qyZZJk2qgH3ZTb33i_IM"
-admins = [6335120359]
+admins = [6335120359, 1041117977]
 
 # All handlers should be attached to the Router (or Dispatcher)
 
